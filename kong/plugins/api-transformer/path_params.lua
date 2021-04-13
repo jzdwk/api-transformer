e3d6c61b7bad5e21b2be7d55e8d71bf494104bad
jiaozhengdayjy <jiaozhengdayjy@tpiot.cmcc>
2021-04-13 15:29:11 +0800

    fix path_params bug

--- kong/plugins/api-transformer/path_params.lua
+++ kong/plugins/api-transformer/path_params.lua
@@ -21,13 +21,13 @@ end
 
 
 -- api_fr_path 即config.api_fr_path的定义, e.g. /req/{id1}/info/{id2}
--- real_path 即实际的请求uri，比如 /cmcc/req/100/info/200
+-- real_path 即实际的请求uri，比如 /cmcc/prefix/req/100/info/200
 -- api_fr_params 即config.api_fr_params，e.g. id1 id2
 -- e.g.
--- api_fr_path = "/get/{id1}/info/{id2}"
--- real_path = "/cmcc/get/100/info/200"
+-- api_fr_path = "/req/{id1}/info/{id2}"
+-- real_path = "/cmcc/prefix/get/100/info/200"
 -- api_fr_params = {"id1","id2"}
--- api_fr_prefix = "cmcc"
+-- api_fr_prefix = "cmcc/prefix"
 -- return {id1=100,id2=200}
 function _M.parse_params(api_fr_path, real_path, api_fr_params, api_fr_prefix)
 	if not api_fr_path or not next(api_fr_params) then
@@ -53,11 +53,11 @@ function _M.parse_params(api_fr_path, real_path, api_fr_params, api_fr_prefix)
 		if string.find(str, "%%") ~= nil then
 			str = decodeURI(str)
 		end
-		--实际path的list, e.g. /req/100/info/200->{req,100,info,200}
+		--实际path的list, e.g. cmcc/prefix/req/100/info/200->{cmcc,prefix,req,100,info,200}
 		table.insert(value_list, str)
 	end
 
-	-- remove path prefix
+	-- remove path prefix {cmcc,prefix,req,100,info,200}->{req,100,info,200}
 	if api_fr_prefix then
 		for k in string.gmatch(api_fr_prefix, "([^/]+)") do	
 			if value_list[API_PATH_PREFIX] ==  k then
@@ -69,11 +69,12 @@ function _M.parse_params(api_fr_path, real_path, api_fr_params, api_fr_prefix)
 
 	--get values from config.api_fr_params e.g. id1,id2
 	for i = 1, #api_fr_params do
+		-- param = id1
 		local param = api_fr_params[i]
 		local pattern = "{" .. param .. "}"
-		--get index of params xxx from reversed keylist 
+		--get index of params xxx from reversed keylist , {req=1,{id1}=2,info=3,{id2}=4}->key_list[{id1}]=2
 		local pos = key_list[pattern]
-		--params_map[id1]=100
+		--params[id1]=100, {req,100,info,200}[2]=100
 		params_map[param] = value_list[pos]
 	end
 
